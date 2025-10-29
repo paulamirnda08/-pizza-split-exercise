21e4d4eed8aa0147ba4c9f9c863cf7d042c64879
paulamirnda08 <paulabelumiranda08@gmail.com>
2025-10-29 17:35:39 -0300

    Update PizzaSlices.java

--- PizzaSlices.java
+++ PizzaSlices.java
@@ -3,35 +3,19 @@ import java.util.Scanner;
 public class PizzaSlices {
   
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
-        int people = 0;
-        int slicesPerPizza = 0;
-
-        while (true) {
-            System.out.print("Enter the number of people: ");
-            people = keyboard.nextInt();
-
-            System.out.print("Enter the number of slices per pizza: ");
-            slicesPerPizza = keyboard.nextInt();
-
-            if (people > 0 && slicesPerPizza > 0) {
-            	
-                break;
-                
-            } else {
-                System.out.println(" Error: both values must be positive! Please try again.\n");
-            }
-        }
+        int people = scanner.nextInt();
+        int slicesPerPizza = scanner.nextInt();
 
         int pizzas = 1;
 
-      
         while ((pizzas * slicesPerPizza) % people != 0) {
             pizzas++;
         }
 
-        System.out.println("Minimum number of pizzas needed: " + pizzas);
+        System.out.println(pizzas);
     }
 }
+
 
